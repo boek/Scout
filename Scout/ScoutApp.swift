2b7b6ec84e3ff404752a8f6aa4236428f7ebb773
Jeff Boek <jeff@jeffboek.com>
2022-08-21 17:25:03 -0700

    Initial Commit

---
+++ Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Jeff Boek on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
