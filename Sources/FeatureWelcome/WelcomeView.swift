5d6f35e752200eb1bdf9ed915dacf380ab6fe5fc
Jeff Boek <jeff@jeffboek.com>
2022-10-01 09:17:53 -0700

    Fixes onboarding styles

--- Sources/FeatureWelcome/WelcomeView.swift
+++ Sources/FeatureWelcome/WelcomeView.swift
@@ -9,6 +9,48 @@ import ComposableArchitecture
 import SwiftUI
 import LibUI
 
+struct CenterAlignedLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Label {
+            configuration.title
+                .alignmentGuide(.leading) {
+                    $0[.leading]
+                }
+        } icon: {
+            configuration.icon
+                .alignmentGuide(.firstTextBaseline) {
+                    $0[VerticalAlignment.center]
+                }
+        }
+    }
+}
+
+struct Instruction {
+    var image: Image
+    var title: String
+    var subtitle: String
+}
+
+struct InstructionView: View {
+    let instruction: Instruction
+
+    var body: some View {
+        Label {
+            VStack(alignment: .leading) {
+                Text(instruction.title)
+                    .font(.headline)
+                    .padding(.bottom, 6)
+                Text(instruction.subtitle)
+                    .font(.subheadline)
+            }
+        } icon: {
+            instruction.image
+                .frame(width: 28)
+                .padding(.trailing, 20)
+        }
+    }
+}
+
 public struct WelcomeView: View {
     public let store: WelcomeStore
 
@@ -17,22 +59,52 @@ public struct WelcomeView: View {
     }
 
     public var body: some View {
-        WithViewStore(store) { a in
+        WithViewStore(store) { viewStore in
             VStack {
-                Text("Welcome to Scout!").font(.headline)
-                Text("Take your browsing to the next level.").font(.subheadline)
+                ScrollView {
+                    VStack(spacing: 24) {
+                        Text("Welcome to Scout!").font(.headline)
+                        Text("Take your browsing to the next level.").font(.subheadline)
 
-                Label("More than just incognito", systemImage: "theatermasks")
+                        VStack(alignment: .leading, spacing: 24) {
+                            InstructionView(
+                                instruction: .init(
+                                    image: Image(systemName: "theatermasks"),
+                                    title: "More than just incognito",
+                                    subtitle: "Scout is a dedicated privacy browser with tracking protection and content blocking"
+                                )
+                            )
 
-                Label("More than just incognito", systemImage: "clock")
+                            InstructionView(
+                                instruction: .init(
+                                    image: Image(systemName: "clock"),
+                                    title: "Your histroy doesn't follow you",
+                                    subtitle: "Erase your browsing history, passwords, cookies, and prevent unwanted ads from following you in a simple click!"
+                                )
+                            )
 
-                Label("Protection at your own discretion", systemImage: "gearshape")
-
-                Spacer()
-                Button(action: { a.send(.startBrowsing) }) {
-                    Text("Start browsing")
-                }.buttonStyle(.primary)
+                            InstructionView(
+                                instruction: .init(
+                                    image: Image(systemName: "gearshape"),
+                                    title: "Protection at your own discretion",
+                                    subtitle: "Configure settings so you can decide how much or how little you share."
+                                )
+                            )
+                        }
+                    }
+                }
+                
+                Button(action: { viewStore.send(.startBrowsing) }) {
+                    HStack {
+                        Spacer()
+                        Text("Start browsing")
+                        Spacer()
+                    }
+                }
+                .buttonStyle(.primary)
+                .frame(minWidth: 0, maxWidth: .infinity)
             }
+            .padding()
         }
     }
 }
